cb098f10c61631fa8ef7bb73718251766bf4141e
tushark1309 <tushar834001@gmail.com>
2023-07-07 21:36:21 +0530

    Add files via upload

--- 4.sort_0_1_2.cpp
+++ 4.sort_0_1_2.cpp
@@ -1,10 +1,52 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+void sort012(int arr[], int n)
+{
+    int start = 0;
+    int end = n - 1;
+    int num = 0;
+    while (num <= end)
+    {
+        if (arr[num] == 0)
+        {
+            {
+                swap(arr[num], arr[start]);
+                num++;
+                start++;
+            }
+        }
+        else if (arr[num] == 2)
+        {
+            {
+                swap(arr[num], arr[end]);
+                end--;
+            }
+        }
+        else
+        {
+            num++;
+        }
+    }
+}
 
+int main()
+{   int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
 
-int main(){
+    sort012(a, n);
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    cout << endl;
 
     return 0;
 }
